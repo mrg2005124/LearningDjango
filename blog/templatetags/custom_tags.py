c7dcb1be6a2596d97f07c9f277d43a0fcf8c54ba
Mohammadrezaghomi <108760608+mrg2005124@users.noreply.github.com>
2024-04-14 23:13:58 +0330

    active admin panel side bar

--- blog/templatetags/custom_tags.py
+++ blog/templatetags/custom_tags.py
@@ -12,3 +12,12 @@ def category_nav():
     return {
         "category" : Category.objects.filter(status = True)
     }
+
+@register.inclusion_tag("registration/partials/link.html")
+def link(request, link_name, content):
+    return {
+        "request" : request,
+        "link_name" : link_name,
+        "link" : f"account:{link_name}",
+        "content" : content
+    }
