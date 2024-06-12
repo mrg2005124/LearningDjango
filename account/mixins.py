bb0f0107af3a6af3cc9e2b967ab66e290f16e18c
Mohammadrezaghomi <108760608+mrg2005124@users.noreply.github.com>
2024-06-12 14:02:17 +0330

    edit mixins

--- account/mixins.py
+++ account/mixins.py
@@ -43,10 +43,14 @@ class AuthorAccessMixin():
         
 class AuthorsAccessMixin():
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_superuser or request.user.is_author:
-            return super().dispatch(request, *args, **kwargs) 
+        if request.user.is_authenticated:
+            if request.user.is_superuser or request.user.is_author:
+                return super().dispatch(request, *args, **kwargs) 
+            else:
+                return redirect('account:profile')
         else:
-            return redirect('account:profile')
+            return redirect('account:login')
+
 
 class SuperuserAccessMixin():
     def dispatch(self, request, *args, **kwargs):
